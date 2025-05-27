6dac3cca3a755005f0cc87d45c760412e81fdec5
Ömer Balyali <omerbalyali@gmail.com>
2025-05-27 14:08:28 +0300

    Add styles to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  modules: ['@nuxt/image'],
+  css: ['~/assets/styles/main.css'],
 })
